b007375b6d5a3b3f87da295958dfa6fc1b080751
Tyler Nguyen <40559762+TylerXNguyen@users.noreply.github.com>
2022-07-14 16:26:19 -0700

    Inital file creation and headers
        Contains start code with comments for creating the 16x16 grid.

---
+++ index.js
@@ -0,0 +1,11 @@
+// const container = document.querySelector(".container");
+// let size = 16;
+// for (let i = 0; i < pixels; i++) {
+//     const pixel = document.createElement("p");
+//     pixel.setAttribute("id", i);
+//     pixel.textContent = i;
+//     container.appendChild(pixel);
+// }
+
+
+const container = document.querySelector(".container");
