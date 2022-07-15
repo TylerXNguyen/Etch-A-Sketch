9e4f048c0dc7a6f93abc29f7e17ea698808739b6
Tyler Nguyen <40559762+TylerXNguyen@users.noreply.github.com>
2022-07-14 17:16:48 -0700

    Corrected hovering over pixel gives random color

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-let size = 16;
+let size = 16; //size x size grid
 const container = document.querySelector(".container");
+// creation of (size * size) grid total size
 for (let j = 0; j < size; j++) {
+    // create for each j row
     for (let i = 1; i <= size; i++) {
-        // for each row
+        // create for each i col
         const pixel = document.createElement("div");
         pixel.classList.add("pixel");
-        pixel.setAttribute("id", i + j * 16);
-        pixel.textContent = i + j * 16;
+        pixel.setAttribute("id", i + j * size);
+        pixel.textContent = i + j * size;
         container.appendChild(pixel);
     }
 }
 
 const pixels = document.querySelectorAll(".pixel");
 pixels.forEach(pixel => {
-    pixel.addEventListener("click", function() {
-        console.log("CLICK");
+    pixel.addEventListener("mouseover", function() {
+        changeRainbow(pixel.id);
         console.log(pixel.id);
+        console.log(randColor());
     });
 });
+
+function randColor() {
+    let color = "#"
+    let hex_letters = "0123456789ABCDEF";
+    for (let i = 0; i < 6; i++) {
+        color += hex_letters[Math.floor(Math.random() * hex_letters.length)];
+    }
+    return color;
+}
+
+function changeRainbow(value_ID) {
+    const draw = document.getElementById(value_ID);
+    draw.style.backgroundColor = randColor();
+}
+
